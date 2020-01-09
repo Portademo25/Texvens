09fb260200ba297db924ef36af50a295058e1b5b
Portademo25 <59373017+Portademo25@users.noreply.github.com>
2020-01-09 16:51:47 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assensts/css/style.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="shortcut icon" href="img/Iconos/Texven New Favicon .ico" >
+   
+    <title>TexVen Cleaning Services</title>
+</head>
+<body  style="border-top-width: 76px;border-top-style: solid;">
+    <?php  include 'partials/header.php'   ?>
+        <div id="slider-container">
+        <div id="slide-0" class="slide fade">
+      <img src="img/Publicidad/bucket-303265_1280.png" alt="Img 0">
+      
+      
+    </div>
+    <div id="slide-1" class="slide fade">
+      <img src="img/Publicidad/tex5.PNG" alt="Img 1">
+    </div>
+    <div id="slide-2" class="slide fade">
+      <img src="img/Publicidad/tex6.PNG" alt="Img 2">
+    </div>
+    <div id="arrows-wrapper">
+      <p id="arrow-prev" class="slider-arrow center_y">
+        &#10094;
+      </p>
+      <p id="arrow-next" class="slider-arrow center_y">
+        &#10095;
+      </p>
+    </div>
+    <div id="dots-wrapper" class="center_x">
+      <div class="dot-nav"></div>
+      <div class="dot-nav"></div>
+      <div class="dot-nav"></div>
+    </div>
+        </div>
+    
+
+    <?php  include 'partials/footer.php'  ?>
+    <script src="js/jquery.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    
+
+</body>
+</html>
